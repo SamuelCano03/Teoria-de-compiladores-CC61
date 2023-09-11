753519ab2cc10456a9b8e1416e03c74a2c1001c6
SamuelCano03 <u202116508@upc.edu.pe>
2023-09-11 00:51:35 -0500

    nums in lexer1, lexer2 in flex

--- week02_lexers/lexer1.c
+++ week02_lexers/lexer1.c
@@ -1,92 +1,83 @@
-//~
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+#include <string.h>
 #define MAXLEXEMA 256
+
 #define COMP 256
 #define ID 257
 #define IF 258
 #define ELSE 259
 #define NUM 260
 
-struct Handler{
-	FILE* file;
-	char lexema[MAXLEXEMA];
+struct Handler {
+  FILE *file;
+  char lexem[MAXLEXEMA];
 };
 
-int lex(struct Handler* h) {
-	size_t i=0;
-	h->lexema[i] = fgetc(h->file);
-
-	
-	if(h->lexema[i]=='<'){
-		h->lexema[++i]=fgetc(h->file);
-		if(h->lexema[i]== '='){
-			h->lexema[++i]= '\0';
-			return COMP;
-		}
-		ungetc(h->lexema[i], h->file);
-		h->lexema[i]='\0';
-		return COMP;
+int lex(struct Handler *h) {
+  size_t i = 0;
+  h->lexem[i] = fgetc(h->file);
+  if (h->lexem[i] == '<') {
+    h->lexem[++i] = fgetc(h->file);
+    if (h->lexem[i] == '=') {
+      h->lexem[++i] = '\0';
+      return COMP;
+    } // else
+    ungetc(h->lexem[i], h->file);
+    h->lexem[i] = '\0';
+    return COMP;
+  } // else
+  if (h->lexem[i] == '>') {
+    h->lexem[++i] = fgetc(h->file);
+    if (h->lexem[i] == '=') {
+      h->lexem[++i] = '\0';
+      return COMP;
+    } // else
+    ungetc(h->lexem[i], h->file);
+    h->lexem[i] = '\0';
+    return COMP;
+  } // else
+  if ((h->lexem[i] >= 'a' && h->lexem[i] <= 'z') ||
+      (h->lexem[i] >= 'A' && h->lexem[i] <= 'Z') || h->lexem[i] == '_') {
+    do {
+      h->lexem[++i] = fgetc(h->file);
+    } while ((h->lexem[i] >= 'a' && h->lexem[i] <= 'z') ||
+             (h->lexem[i] >= 'A' && h->lexem[i] <= 'Z') ||
+             (h->lexem[i] >= '0' && h->lexem[i] <= '9') || h->lexem[i] == '_');
+    h->lexem[i] = '\0';
+    ungetc(h->lexem[i], h->file);
+    if (strcmp(h->lexem, "if") == 0) {
+      return IF;
+    } else if (strcmp(h->lexem, "else") == 0) {
+      return ELSE;
+    }
+    return ID;
+  }
+  if (h->lexem[i] >= '0' && h->lexem[i] <= '9') {
+    do {
+      h->lexem[++i] = fgetc(h->file);
+    } while (h->lexem[i] >= '0' && h->lexem[i] <= '9');
+    h->lexem[i] = '\0';
+    ungetc(h->lexem[i], h->file);
+		return NUM;
 	}
-	if(h->lexema[i]=='>'){
-		h->lexema[++i]=fgetc(h->file);
-		if(h->lexema[i]== '='){
-			h->lexema[++i]= '\0';
-			return COMP;
-		}
-		ungetc(h->lexema[i], h->file);
-		h->lexema[i]='\0';
-		return COMP;
-	}
-	if(
-		(h->lexema[i]>= 'a' && h->lexema[i]<='z')
-		||
-		(h->lexema[i]>= 'A' && h->lexema[i]<='Z')
-		|| h->lexema[i]=='_'
-	){
-		do{
-			h->lexema[++i]=fgetc(h->file);
-		}while((h->lexema[i]>= 'a' && h->lexema[i]<='z')
-		||
-		(h->lexema[i]>= 'A' && h->lexema[i]<='Z')
-		|| h->lexema[i]=='_' || 
-		(h->lexema[i]>='0' && h->lexema[i]<='9')
-		);
-		h->lexema[i]='\0';
-		ungetc(h->lexema[i], h->file);
-		if(strcmp(h->lexema, "if")==0)
-			return IF;
-		else if (strcmp(h->lexema, "else")==0) 
-			return ELSE; 
-		else return ID;
-	}
-	return h->lexema[i]!=EOF?lex(h):EOF;
+  return h->lexem[i] != EOF ? lex(h) : EOF;
 }
 
-int main(int argc, char*argv[]){
-	if(argc<2){
-		printf("Usage: %s <sourcefile>\n", argv[0]);
-		return -1;
-	}
-	
-	struct Handler* handler = (struct Handler*) malloc(sizeof(struct Handler));
-	handler->file=fopen(argv[1],"r");
-	int tok;
-	while((tok=lex(handler))!=EOF){
-		printf("Lexem (%d): %s\n", tok,handler->lexema);
-	}
-	fclose(handler->file);
-	free(handler);
-	return 0;
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("Usage: %s <sourcefile>\n", argv[0]);
+    return -1;
+  }
+  struct Handler *handler = (struct Handler *)malloc(sizeof(struct Handler));
+  handler->file = fopen(argv[1], "r");
+
+  int tok;
+  while ((tok = lex(handler)) != EOF) {
+    printf("Lexem (%d): %s\n", tok, handler->lexem);
+  }
+
+  fclose(handler->file);
+  free(handler);
+  return 0;
 }
-/*
-//~
-<
-<=
->
->=
-!=
-==
-*/
-//~
