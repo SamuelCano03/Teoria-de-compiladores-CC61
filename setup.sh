db2ffcced20c475d3992b87c5f0e4b212405251f
SamuelCano03 <u202116508@upc.edu.pe>
2023-09-25 00:32:26 -0500

    agregando el setup.sh actualizado

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/zsh
-
-alias grun = 'java org.antlr.v4.gui.TestRig'
-
+export CLASSPATH=.:~/.m2/repository/org/antlr/antlr4/4.13.1/antlr4-4.13.1-complete.jar:$CLASSPATH
+alias grun='java org.antlr.v4.gui.TestRig'
