7897af343e578c8e3f2ed4d9ac3b0c64cff77bd9
SamuelCano03 <u202116508@upc.edu.pe>
2023-09-27 07:38:27 -0500

    jeje

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/zsh
+source $HOME/Documents/Teoria-de-compiladores-CC61/myenv/bin/activate
 export CLASSPATH=.:~/.m2/repository/org/antlr/antlr4/4.13.1/antlr4-4.13.1-complete.jar:$CLASSPATH
 alias grun='java org.antlr.v4.gui.TestRig'
