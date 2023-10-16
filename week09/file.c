06f49da4b5e0708de1a921717732dc89a570b2a4
SamuelCano03 <u202116508@upc.edu.pe>
2023-10-16 16:54:51 -0500

    semana 9

---
+++ week09/file.c
@@ -0,0 +1,36 @@
+/**
+ * this file is to be used as a test of clang IL generation capabilities
+ * compile as:
+ *   clang file.c: error because there is no main (entry point)
+ *   clang -S file.c: generates file.s which contains the code in ASM
+ *   clang -S -emit-llvm filec.: generates file.ll which cotains llvm IR code
+ */
+
+void prefix_sum(int *src, int *dst, int N) {
+	/* for(int i=0;i<N;i++){
+		int tmp=0;
+		int j=0;
+		if(j<i){
+			do {
+				tmp += src[j];
+				j++;
+			}while(j<i);
+			dst[i]=tmp;
+		}
+	} */
+	if (0 < N) {
+		int i = 0;
+		do {
+			int tmp = 0;
+			int j = 0;
+			if (j < i) {
+				do {
+					tmp += src[j];
+					j++;
+				} while (j < i);
+				dst[i] = tmp;
+			}
+			i++;
+		} while (i < N);
+	}
+}
