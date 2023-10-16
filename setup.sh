a3dc2f994e22044dcf122107935ec6269702c360
SamuelCano03 <u202116508@upc.edu.pe>
2023-10-16 08:01:26 -0500

    update my setup

--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@
 source $HOME/Documents/Teoria-de-compiladores-CC61/myenv/bin/activate
 export CLASSPATH=.:~/.m2/repository/org/antlr/antlr4/4.13.1/antlr4-4.13.1-complete.jar:$CLASSPATH
 alias grun='java org.antlr.v4.gui.TestRig'
+
+export ANTLR4I=/home/samuel/MyApps/antlr4-cpp/run/usr/local/include
+export ANTLR4L=/home/samuel/MyApps/antlr4-cpp/build/runtime
